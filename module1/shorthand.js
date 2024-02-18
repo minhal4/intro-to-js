4584fcddfd987646a5ddaddfb1f0fc4d9d049dde
minhal4 <minhalahmed222@gmail.com>
2024-02-19 02:27:34 +0600

    seven

---
+++ module1/shorthand.js
@@ -0,0 +1,27 @@
+var tomatoPrice = 25; //variable dec
+tomatoPrice =40; //variable updated
+
+// add anything new to variable
+var price1 =31;
+var price2 =20;
+
+price1 = price1 - 20;
+// console.log(price1)
+
+// shorthand variable update
+
+price1 += 10;
+
+console.log(price1);
+
+price2 *= 5;
+console.log(price2);
+
+price2 /= 20;
+
+console.log(price2);
+price1++;
+price2--;
+
+console.log(price1);
+console.log(price2);
