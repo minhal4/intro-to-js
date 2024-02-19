7de570df0148ad10063ce659cd7b3f0cfd7b2f6d
minhal4 <minhalahmed222@gmail.com>
2024-02-19 21:34:30 +0600

    module-one-done

---
+++ module1/concatenation.js
@@ -0,0 +1,16 @@
+var price1 = 20;
+var price2 = 35;
+var totalPrice = price1 + price2; 
+// console.log(totalPrice);
+
+// eto somoy amra zto output deksi sob numeric .. what if output is a string and letter type  
+
+var firstName = "minhal";
+var surname = "Abir";
+var fullName = firstName + ' '+ surname;
+console.log(fullName);
+
+// ei kali jayga acquire kora ke concatenation bole 
+
+
+
