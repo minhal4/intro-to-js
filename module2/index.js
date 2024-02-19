191fdbf7b94ccbfbd88d590fa99c38ab355b9083
minhal4 <minhalahmed222@gmail.com>
2024-02-19 23:54:27 +0600

    indexDetails

---
+++ module2/index.js
@@ -0,0 +1,15 @@
+// 1.index number diye element ber kora 
+ var numb = [10 , 20, 30 , 40, 50 , 60];
+ console.log(numb[3]);
+
+// 2 . array er kuno certain index er value inject kora;
+
+ var numb = [10 , 20, 320 , 40, 50 , 60];
+ numb[2] = 31;
+ console.log(numb[2]);
+
+// 3/ Element jana ase koto number index jana nai
+
+var numb = [10 , 20, 30 , 40, 50 , 60];
+var positionOF = numb.indexOf(30);
+console.log(positionOF);
