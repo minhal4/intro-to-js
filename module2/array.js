e493a990d3058cc57374810f175a8ec22741e8e5
minhal4 <minhalahmed222@gmail.com>
2024-02-19 23:23:02 +0600

    index

---
+++ module2/array.js
@@ -0,0 +1,5 @@
+var friendsAge = [11 ,21 ,45 ,17, 14 ,105 ,6];
+var picnicFee = [5000,2000, 4000, 150];
+var heroines = ['mahiya mahi','opu bissas' , 'shabana'];
+var vowerls =['a', 'e', 'i' ,'o', 'u'];
+console.log(friendsAge.length);
