c8ba31b158fc2f82f7d6ba8b06faac092d5f918a
minhal4 <minhalahmed222@gmail.com>
2024-02-21 20:42:27 +0600

    rewind

---
+++ module2/module-rewind.js
@@ -0,0 +1,20 @@
+  var pracArray = ["kolkata","pakistan","nepal", "indonesia"];
+   pracArray[2] = "KASHMIR";
+  console.log(pracArray[2])
+  var position = pracArray.indexOf("pakistan");
+  console.log(position);
+  pracArray.push("coxsBazar") ;
+  pracArray.unshift("badarBan")
+  pracArray.pop();
+  pracArray.shift();
+  console.log(pracArray)
+//   home works 
+
+
+ var fruits = [ 'apple','banana','orange'];
+ var position = fruits.indexOf("banana")
+ console.log(position);
+ fruits.pop();
+ fruits.push("watermelon");
+ console.log(fruits);
+
