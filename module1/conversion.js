ba7319e91604bb6aa58e172367590573c29f92ac
minhal4 <minhalahmed222@gmail.com>
2024-02-21 11:43:48 +0600

    practice_done

--- module1/conversion.js
+++ module1/conversion.js
@@ -1,8 +1,9 @@
-var number = '99';
+var number = "99";
 
-var num = '88';
+var num = "88";
 console.log(number + num);
-var num1 =parseInt(number) ;
+var num1 = parseInt(number);
 console.log(num1);
-var num2 =parseInt(num);
+var num2 = parseInt(num);
 console.log(num2);
+
---
+++ module1/practice@#$%.js
@@ -0,0 +1,6 @@
+var came = '10';
+var fame ='12';
+var name1 = parseInt(came);
+var fame1 = parseInt(fame);
+var amount = (name1 +fame1);
+console.log(amount);
