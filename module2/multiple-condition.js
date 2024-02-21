671273aa2187db764f6aaeb027a335da692b7445
minhal4 <minhalahmed222@gmail.com>
2024-02-21 19:23:20 +0600

    multiple_condition

---
+++ module2/multiple-condition.js
@@ -0,0 +1,26 @@
+// var graduated = true;
+// var salary = 350000;
+// var cars = 0
+
+
+// // if(graduated == true) {
+// //     console.log("sikkito");
+// // }
+// // else{
+// //     console.log("tor kopale jutar bari");
+// // }
+// if(graduated == true && salary> 50000 && cars >= 1) {
+//     console.log("esho basay eso");
+// }
+// else{
+//     console.log("tor kopale jutar bari");
+// }
+var graduated = true;
+var salary = 350000;
+var cars = 3;
+if( (graduated == true || salary > 500000) &&  cars >= 1) {
+    console.log("esho basay eso");
+}
+else {
+    console.log("tor kopale jutar bari");
+}
