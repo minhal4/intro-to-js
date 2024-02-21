580ba1d9c94cbc7fdd9fc63e151a2ce1dc9bf2de
minhal4 <minhalahmed222@gmail.com>
2024-02-21 13:59:53 +0600

    comparsion

---
+++ module2/comparsion.js
@@ -0,0 +1,11 @@
+// console.log(5<6);
+// console.log(1>7);
+// console.log(15==15);
+// console.log(17==7);
+// console.log(15 != 5);
+// console.log(17 != 17);
+console.log(5<=6);
+console.log(5 >= 6);
+
+//multiple conditional statements:  
+
