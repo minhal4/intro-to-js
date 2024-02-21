0a0d9c6f49b5af887252475e3332016f77018a96
minhal4 <minhalahmed222@gmail.com>
2024-02-21 18:59:20 +0600

    conditional

--- module2/comparsion.js
+++ module2/comparsion.js
@@ -8,4 +8,5 @@ console.log(5<=6);
 console.log(5 >= 6);
 
 //multiple conditional statements:  
-
+money1 > money2 && result1> result2 && bari1 > bari2
+taka1 > taka2 || fol1 > fol2 || deer1 >deer2
---
+++ module2/conditional.js
@@ -0,0 +1,8 @@
+var iphonePrice = 790000;
+var myBudget = 95000;
+if(iphonePrice<myBudget) {
+    console.log('Iphone is my');
+}
+else{
+    console.log('nokia is mine');
+}
