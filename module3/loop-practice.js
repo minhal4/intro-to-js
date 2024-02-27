df08300a3d75137d4177d07727c5c5f4ec194745
minhal4 <minhalahmed222@gmail.com>
2024-02-27 19:23:34 +0600

    back

---
+++ module3/loop-practice.js
@@ -0,0 +1,40 @@
+// // ajk amr mon valo nai 39 times print krte hobe
+//  for(var nar= 0; nar<40;nar++){
+//      console.log(nar)
+//      console.log('aj amar mon valo nei');
+//  }
+//  var nar = 0;
+//  while(nar <40){  
+//      console.log('aj amr mon valo nei mama')
+//      console.log(nar)
+//      nar +=1;
+//  }
+// ??
+// for(var nar = 58; nar <99 ; nar +=1)
+// {
+//     console.log(nar)
+// }
+
+// show all even numbers between 412 to 456;
+// for (var init = 412 ; init <= 456 ; init +=2 ){
+//     console.log(init)
+// }
+
+// show all even numbers between 581 to 623;
+// for(var init = 581; init <= 623 ; init +=2)
+// {
+// console.log(init)
+// }
+
+//display an array
+// var array=['html','vanilla', 'bootstrap','tailwind'];
+// for(x=0;x<array.length;x+=1){
+//     xray = array[x];
+//     console.log(xray)
+// }
+
+// var positionOF = array.indexOf('vanilla')
+// console.log(positionOF)
+
+// while 
+
---
+++ module3/rewind.js
@@ -0,0 +1,41 @@
+// video 1
+// var num = 0;
+// while(num < 10){
+//     console.log("world is worse than my thinkings");
+//     console.log(num)
+//     num += 2;
+// }
+
+// video 2
+// for(var x = 0; x<10 ; x++){
+    
+//     console.log("world is worse than my thinkings");
+//     console.log(x)
+// }
+// VIDEO --4
+ var vid = ['video 1', 'video 2', 'video 3', 'video 4 ', 'video 5', 'video 6']
+   for( x = 0 ; x<vid.length; x++ ){
+     var world = vid[x]
+     
+     if( x > 2){
+         break;
+     }
+     console.log(world);
+   }
+
+// var vid = ['video 1', 'video 2', 'video 3', 'video 4 ', 'video 5', 'video 6']
+
+// for(var x = 0; x<vid.length; x++){
+//     var cham = vid[x];
+    
+//     if(x==1){
+//         continue;
+//     }
+//     console.log(cham);
+// }
+  
+// vid--- 7 reverse of for and while loop;
+for(var x = 10; x>=1; x-- )
+{
+    console.log(x);
+}
